4d94df954cb734763d24901992101ed27f2c5534
carlo rothschild <carlorothschild@thors-hammer.home>
2020-05-12 22:32:49 -0400

    Mission Complete

--- src/components/AdditionalFeatures.js
+++ src/components/AdditionalFeatures.js
@@ -7,8 +7,12 @@ const AdditionalFeatures = props => {
       <h4>Additional Features</h4>
       {props.additionalFeatures.length ? (
         <ol type="1">
-          {props.additionalFeatures.map(item => (
-            <AdditionalFeature key={item.id} feature={item} />
+          {props.additionalFeatures.map((item) => (
+            <AdditionalFeature
+              key={item.id}
+              feature={item}
+              buyItem={props.buyItem}
+            />
           ))}
         </ol>
       ) : (
